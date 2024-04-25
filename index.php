e5333db6467d584df45d5b80f89c544ce92c7426
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-04-25 16:57:57 +0200

    updt

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
-/* $_Get est une superglobale. 
-une superglobale n'a pas besoin d'être créee, elle est automatiquement chargée par le serveur PHP. Les superglobales sont accessibles dans toutes les portées.
+/* 
+    $_Get est une superglobale. 
+Une superglobale n'a pas besoin d'être créee, elle est automatiquement chargée par le serveur PHP. 
+Les superglobales sont accessibles partout, depuis n'importe quelle fonction classe ou fichier, sans rien avoir à faire de spécial.
+
+*/
