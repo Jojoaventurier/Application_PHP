c347c904e1ecb0a64be57c7bd04ca112a27f4e0b
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-04-26 15:45:34 +0200

    finished the guided exercise and added some notes to the lesson part about cookies

--- recap.php
+++ recap.php
@@ -26,20 +26,26 @@
                         "</tr>",
                     "</thead>",
                     "<tbody>";
-
+            $totalGeneral = 0;
             foreach($_SESSION['products'] as $index => $product) {                                           // Ligne 30, on observe la boucle itérative foreach() de PHP, particulièrement efficace pour éxectuer, produit par produit, les mêmes instructions qui vont permettre l'affichage uniforme de chacun d'entre eux. Pour chaque donnée dans $_SESSION['products'], nous disposeront au sein de la boucle de deux variables : $index = aura pour valeur l'index du tableau $_SESSION['products'] parcouru. Nous pourrons numéroter ainsi chaque produit avec ce numéro dans le tableau HTML (en première colonne), et $product = cette variable contiendra le produit, sous forme de tableau, tel que l'a créé et stocké en session le fichier traitement.php 
                 echo "<tr>",
-                        "<td>".$index."</td>",
+                         "<td>".$index."</td>",
                         "<td>".$product['name']."</td>",                                                 // la boucle créera alors une ligne <tr> et toutes les cellules <td> nécessaires à chaque partie du produit à afficher, et ce pour chaque produit présent en session. 
-                        "<td>".number_format($product['price'], 2, ",", "&nbsp;"). "nbsp;€</td>",           
-                        "<td>".number_format($product['qtt'], 2, ",", "&nbsp;")."&nbsp;€</td>",          // La fonction number_format() permet de modifier l'affichage d'une valeur numérique en précisant plusieurs paramètres : number_format(variable à modifier, nb de décimales souhaité, caractère séparateur décimal, caractère séârateur de milliers); 
-                        "<td>".$product['total']."</td>",                                                  // en ajoutant le symbole €,  avant la fermeture le la balise <td>, le prix s'affichera avec € derrière
+                        "<td>".number_format($product['price'], 2, ",", "&nbsp;"). "&nbsp;€</td>",  
+                        "<td>".$product['qtt']."</td>",                                                     // La fonction number_format() permet de modifier l'affichage d'une valeur numérique en précisant plusieurs paramètres : number_format(variable à modifier, nb de décimales souhaité, caractère séparateur décimal, caractère séârateur de milliers); 
+                        "<td>".number_format($product['total'], 2, ",", "&nbsp")."&nbsp;€</td>",               // en ajoutant le symbole €,  avant la fermeture le la balise <td>, le prix s'affichera avec € derrière
                     "</tr>";
-            }
-            echo "</tbody>",
-                "</table>";
+                $totalGeneral += $product['total'];
+            } 
+            echo "<tr>",
+                    "<td colspan=4>Total général : </td>",
+                    "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                "</tr>",
+            "</tbody>",
+            "</table>";
         }
    
+     //var_dump($_SESSION['products']);   
    ?> 
 </body>
 </html>
