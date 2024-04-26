b5c3549d6b4661b0d77b207dfcd0709a9ef58d4b
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-04-26 15:02:04 +0200

    updt

--- traitement.php
+++ traitement.php
@@ -3,17 +3,17 @@
 
     if(isset($_POST['submit'])){   //avec cette condition, on vérifie l'existence de la clé "submit" dans le tableau $_POST, cette clé correspond à l'attribut "name" du bouton <input type="submit" name="submit"> du formulaire  //La condition sera vraie seulement si la requête POST tranmet bien une clé "submit" au serveur. 
          
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING); // FILER_SANITIZE_STRING (champ name) : ce filtre supprime une chaîne de caractères de toute présence de caractère spéciaux, et de toute balise HMTL potentielle ou les encode. Pas d'injection de code HTML possible !
-        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); // FILTER_VALIDATE_FLOAT (champ "price") : ne validera le prix que s'il est un nombre à fraction (pas de texte ou autre...), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale. 
-        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT); // FILTER_VALIDATE_INT (champ "qtt") : ne validera la quantité que si celle-ci est un nb entier différent de zéro (qui est considéré come null)
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING); // FILER_SANITIZE_STRING (champ name) : ce filtre supprime une chaîne de caractères de toute présence de caractère spéciaux, et de toute balise HMTL potentielle ou les encode. Pas d'injection de code HTML possible !
+        $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); // FILTER_VALIDATE_FLOAT (champ "price") : ne validera le prix que s'il est un nombre à fraction (pas de texte ou autre...), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale. 
+        $qtt = filter_input(INPUT_POST, 'qtt', FILTER_VALIDATE_INT); // FILTER_VALIDATE_INT (champ "qtt") : ne validera la quantité que si celle-ci est un nb entier différent de zéro (qui est considéré come null)
                                              // filer_input() renvoie en cas de succès la valeur assainie correspondant au champ traité, false si le titre échoue ou null si le champs sollicité par le nettoyage    n'existait pas dans la requête POST. Ainsi, PAS DE RISQUES QUE L'UTILISATEUR TRANSMETTE DES CHAMPS SUPPLEMENTAIRES ! A la suite de cela, nous disposons de toirs variables $name, $price et $qtt, censées contenir respectivement les valeurs nettoyées et/ou validées du formulaire.
         if ($name && $price && $qtt) {              //Il faut maintenant vérifier si les filtres ont tous fonctionné grâce à une nouvelle condition : Sachant quun filtre renverra false ou null s'il échoue, et que nous ne pouvons pas ancitiper la saisie de l'utilisateru à ce niveau, il suffit de vérifier implicitement si chaque variable contient une valeur jugée positive par PHP (du texte, des nombres etc., autrement dit tout sauf false, null ou 0). Voilà pourquoi la condition ne compare les variables  àrien de précis. 
 
             $product = [  // $product comporte, comme stipulé dans le cahier des charges, une 4ème valeur "total", résultat de la multiplication du prix par la quantité du produit. Cette organisation des données nous permettra d'afficher nos produits plus tard de manière plus efficace et de rendre notre code plus explicite.  //Maintenant, il ne nous reste plus qu'à enregistrer ce produit en nouvellement créé en session. 
-                "name" => $name,
-                "price" => $price,
-                "qtt" => $qtt,
-                "total" => $price*$qtt
+                'name' => $name,
+                'price' => $price,
+                'qtt' => $qtt,
+                'total' => $price*$qtt
             ];
 
             $_SESSION["products"][] = $product; // cette ligne est efficace car : -> on sollicita le tableau de session $_SESSION fourni par PHP. -> on indique la clé "products" de ce tableau. Si cette clé n'existait pas auparavant (par exemple si l'utilisateur ajoute son tout premier produit), PHP la crééera au sein de $_SESSION. -> les deux crochets [] sont un raccourci pour indiquer à cet emplacement que nous ajoutons une nouvelle entrée au futur tableau "products" associé à cette clé. $_SESSION["products"] doit être lui aussi un tableau afin d'y stocker de nouveaux produits par la suite. (autre syntaxe pour $_SESSION['products'][] = $product est : array_push($_SESSION['products'], $product) mais PHP nous conseille dans sa documentation d'utiliser les [] pour éviter le passage d'une fonction (plus lourd en termes de performances)
